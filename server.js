1411ac6ed64d7690d5ade8b467d9a93a7261a9a9
arcurrated <lindorn@bk.ru>
2022-10-02 23:24:32 +0300

    update)

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const dotenv = require('dotenv')
 
 dotenv.config({ path: '.env.local' })
 
+const five = require('johnny-five');
+const board = new five.Board({
+	repl: false,
+});
+
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TG_TOKEN;
 
@@ -15,16 +20,45 @@ const isAdmin = (tgId) => {
 }
 
 // 💡🔔🔕
+let relay1 = null
+let relay2 = null
+
 const currentState = {
     relay1: {
         title: '💡 Основной свет',
         isActive: false,
+        toggle: () => {
+            if(relay1 !== null){
+                if(currentState.relay1.isActive){
+                    relay1.close()
+                } else {
+                    relay1.open()
+                }
+            }
+            currentState.relay1.isActive = !currentState.relay1.isActive
+        }
     },
     relay2: {
         title: '💡 Бра',
         isActive: false,
+        toggle: () => {
+            if(relay2 !== null){
+                if(currentState.relay2.isActive){
+                    relay2.close()
+                } else {
+                    relay2.open()
+                }
+                //relay2.toggle()
+            }
+            currentState.relay2.isActive = !currentState.relay2.isActive
+        }
     }
 }
+
+board.on('ready', function(){
+    relay1 = new five.Relay(7)
+    relay2 = new five.Relay(8)
+});
 
 const getKeyboard = () => {
     const keyboard = []
@@ -66,9 +100,13 @@ bot.onText(/\/start/, (msg, match) => {
 // messages.
 
 bot.on('message', (msg) => {
+    //console.log(msg)
     const chatId = msg.chat.id;
     if(!isAdmin(chatId)){
         return bot.sendMessage(chatId, 'Forbidden')
+    }
+    if(msg.sticker){
+        return bot.sendMessage(chatId, msg.from.first_name + ', не балуйся!')
     }
     if(msg.text == '/start'){
         return
@@ -78,7 +116,7 @@ bot.on('message', (msg) => {
     for(let i in currentState){
         const val = currentState[i]
         if(text.indexOf(val.title) !== -1){
-            currentState[i].isActive = !currentState[i].isActive // toggle
+            currentState[i].toggle()
             isDone = true
         }
     }
@@ -92,3 +130,12 @@ bot.on('message', (msg) => {
         reply_markup: getKeyboard(),
     });
 });
+
+bot.on('polling_error', (err) => {
+    console.log(err)
+    console.log(relay1)
+})
+
+process.on('uncaughtException', function(err){
+	console.log(err);
+})
